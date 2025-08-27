0dbbea53cd8d61e2df10f8a9efa5a3deb2df0df0
donghunee <ehdgns1766@naver.com>
2025-08-27 11:14:18 +0900

    docs: 코파일럿 활용 방법에 대한 예시 추가 및 개선

---
+++ 0/main.js
@@ -0,0 +1,13 @@
+function example() {
+  console.log('This is an example function.');
+}
+
+// src/app.js
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+function a() {
+  console.log('Function a');
+}
